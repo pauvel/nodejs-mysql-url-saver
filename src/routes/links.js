bfee43646e5947c6761c7374cf184475aceedaed
Paul G <paulsystem32@gmail.com>
2020-08-04 20:36:07 -0600

    Primera vista & utils
    - Se agregaron las utilidades que vamos a utilizar directamente en el
      main.
    - Se hizo uso de la primera vista hecha con handlebars.
    - Se iimplemento la ruta para agregar un nuevo link /add.

--- src/routes/links.js
+++ src/routes/links.js
@@ -2,5 +2,12 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database'); // Recibimos el objeto pool.
 
+router.get('/add', (req, res) =>{
+    res.render('links/add')
+});
+
+router.post('/add', (req, res) => {
+    res.send('recibido');
+});
 
 module.exports = router;
